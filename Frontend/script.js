a253f3485b97d1da5bc6d9053b35d93313d7a379
Tejus Jaiswal <tejusjaiswal13@gmail.com>
2026-02-07 12:45:22 +0530

    Hosting change

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,5 +1,6 @@
 // ===== CONFIGURATION =====
-const BACKEND_URL = "http://localhost:5000";
+const BACKEND_URL = "http://127.0.0.1:5000";
+
 const API_BASE = `${BACKEND_URL}/api/v1/legal`;
 
 // ===== DOM ELEMENTS =====
